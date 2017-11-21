bad1de822d17979f7fd07bdc89dbfa64af63311d
Starycek <natojebu@gmail.com>
2017-11-21 20:10:00 +0100

    Fce update
    fce.h

--- funkce.h
+++ funkce.h
@@ -3,3 +3,10 @@ typedef struct{
 	int lenght;
 	int size;
 } string;
+
+int String_Init(string *s);
+int String_Add_Char(string *string, char c);
+int String_Add_Char_Lower(string *string, char c);
+void String_Delete_Char(string *string);
+void String_Clear (string *s);
+void String_Delete (string *s);
