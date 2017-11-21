02ab91170784914f4a6b2bebf708e58018a0a6f1
stasamuradio <33875151+stasamuradio@users.noreply.github.com>
2017-11-21 20:54:09 +0100

    scanner.h
    izi katuska

---
+++ scanner.h
@@ -0,0 +1,54 @@
+#include "main.h"
+#include "funkce.h"
+
+//OPERATORY
+#define DOUBDIV 10 // '/'
+#define SUB 11 // '-'
+#define MULT 12 // '*'
+#define ADD 13 // '+'
+#define INTDIV 14 // '\'
+#define EQL 15 // '='
+#define GREQ 16 // '>='
+#define GREATR 17 // '>'
+#define LESEQ 18 // '<='
+#define LESSER 19 // '<'
+#define DIFFERENT 20 // '<>'
+#define COMM 21 // ','
+#define SEMICOL 22 // ';'
+#define RIGHTBRAC 23 // ')'
+#define LEFTBRAC 24 // '('
+
+//TYPY
+#define ID 30 // 'identifikator'
+#define TEXT 31 // 'string'
+#define INTNUM 32 // 'integer'
+#define COMMENT 33 // 'komentar'
+#define DOUBNUM 34 // 'double'
+#define STRINGSTART 35 // 'zacatek stringu'
+
+//klicova slova
+#define AS 40
+#define ASC 41
+#define DECLARE 42
+#define DIM 43
+#define DOO 44  //do
+#define DOUBL 45 //double
+#define ESLE 46
+#define EDN 47 //end
+#define CHR 48
+#define FUNCTION 49
+#define FI 50 //if
+#define INPUT 51
+#define INTEGER 52
+#define LENGHT 53
+#define LOOP 54
+#define PRINT 55
+#define RETURN 56
+#define SCOPE 57
+#define STRENK 58 //string
+#define SUBSTRA 59 //substr
+#define THEN 58
+#define WHIL 59 //while
+
+int getNextToken(string *s)
+#endif
