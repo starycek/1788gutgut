87cf8a22505f82199294242f2aa86859128110f3
stasamuradio <33875151+stasamuradio@users.noreply.github.com>
2017-11-22 16:41:39 +0100

    scanner

--- scanner.c
+++ scanner.c
@@ -13,14 +13,11 @@
 
 FILE *source;
 int GetNextToken(){
-string *s = NULL;
 char c;
 int state=0;
 int radek=1;
-String_Init (s);
-String_Clear(s);
-
-printf("%s",s->str);
+int i=0;
+token[i]='\0';
 while(1){
   c=getc(source);
   if(feof(source)){
@@ -36,63 +33,99 @@ while(1){
       state=0;
     }
     else if(c=='/'){
+      token[i]=c;
+      i++;
       return DOUBDIV;
     }
     else if(isalpha(c)||c=='_'){
-      String_Add_Char_Lower(s,c);
+      token[i]=c;
+      i++;
       state=2;
     }
     else if(isdigit(c)){
-      String_Add_Char_Lower(s,c);
+      token[i]=c;
+      i++;
       state=3;
     }
     else if (c=='-'){
+      token[i]=c;
+      i++;
       return SUB;
     }
     else if (c=='\\'){
+      token[i]=c;
+      i++;
       state=1;
     }
       else if (c=='*'){
+      token[i]=c;
+      i++;
       return MULT;
     }
       else if (c=='+'){
+      token[i]=c;
+      i++;
       return ADD;
     }
       else if (c=='!'){
-          return STRINGSTART;
+      token[i]=c;
+      i++;
+      return STRINGSTART;
     }
       else if (c=='='){
-          return EQL;
+      token[i]=c;
+      i++;
+      return EQL;
     }
       else if(c=='>'){
+      token[i]=c;
+      i++;
       if((c=getc(source))=='='){
+        token[i]=c;
+        i++;
         return GREQ;
       } else{
       return GREATR;
       }
       }
       else if(c=='<'){
+        token[i]=c;
+        i++;
       if((c=getc(source))=='='){
+        token[i]=c;
+        i++;
         return LESEQ;
       } else if((c=getc(source))=='>'){
-      return DIFFERENT;
+        token[i]=c;
+        i++;
+        return DIFFERENT;
       }else{
       return LESSER;
       }
     }
     else if(c==','){
+      token[i]=c;
+      i++;
       return COMM;
     }
     else if(c==';'){
+      token[i]=c;
+      i++;
       return SEMICOL;
     }
     else if(c==')'){
+      token[i]=c;
+      i++;
       return RIGHTBRAC;
     }
     else if(c=='('){
+      token[i]=c;
+      i++;
       return LEFTBRAC;
       }
     else if(c=='"'){
+      token[i]=c;
+      i++;
       state=4;
     }
     else{
@@ -102,8 +135,11 @@ while(1){
 
   case 1:
     if(c=='\''){
+      token[i]=c;
+      i++;
       state=6;
     }else if(isdigit(c)){
+    ungetc(c,source);
     return INTDIV;
     }else{
     return 1;
@@ -111,53 +147,55 @@ while(1){
     break;
   case 2:
     if(isalnum(c) || c=='_'){
-      String_Add_Char_Lower(s,c);
+      token[i]=c;
+      i++;
       break;
     }else{
-      ungetc(c,source);
-    if(strcmp(s->str,"as")==0){
+      token[i]=c;
+      i++;
+    if(strcmp(token,"as")==0){
       return AS;
-    }else if(strcmp(s->str,"asc")==0){
+    }else if(strcmp(token,"asc")==0){
       return ASC;
-    }else if(strcmp(s->str,"declare")==0){
+    }else if(strcmp(token,"declare")==0){
       return DECLARE;
-    }else if(strcmp(s->str,"dim")==0){
+    }else if(strcmp(token,"dim")==0){
       return DIM;
-    }else  if(strcmp(s->str,"do")==0){
+    }else  if(strcmp(token,"do")==0){
       return DOO;
-    }else if(strcmp(s->str,"double")==0){
+    }else if(strcmp(token,"double")==0){
       return DOUBL;
-    }else if(strcmp(s->str,"else")==0){
+    }else if(strcmp(token,"else")==0){
       return ESLE;
-    }else if(strcmp(s->str,"end")==0){
+    }else if(strcmp(token,"end")==0){
       return EDN;
-    }else  if(strcmp(s->str,"chr")==0){
+    }else  if(strcmp(token,"chr")==0){
       return CHR;
-    }else if(strcmp(s->str,"function")==0){
+    }else if(strcmp(token,"function")==0){
       return FUNCTION;
-    }else if(strcmp(s->str,"if")==0){
+    }else if(strcmp(token,"if")==0){
       return FI;
-    }else if(strcmp(s->str,"input")==0){
+    }else if(strcmp(token,"input")==0){
       return INPUT;
-    }else  if(strcmp(s->str,"integer")==0){
+    }else  if(strcmp(token,"integer")==0){
       return INTEGER;
-    }else if(strcmp(s->str,"lenght")==0){
+    }else if(strcmp(token,"lenght")==0){
       return LENGHT;
-    }else if(strcmp(s->str,"loop")==0){
+    }else if(strcmp(token,"loop")==0){
       return LOOP;
-    }else if(strcmp(s->str,"print")==0){
+    }else if(strcmp(token,"print")==0){
       return PRINT;
-    }else  if(strcmp(s->str,"return")==0){
+    }else  if(strcmp(token,"return")==0){
       return RETUNR;
-    }else if(strcmp(s->str,"scope")==0){
+    }else if(strcmp(token,"scope")==0){
       return SCOPE;
-    }else if(strcmp(s->str,"string")==0){
+    }else if(strcmp(token,"string")==0){
       return STRENK;
-    }else if(strcmp(s->str,"substr")==0){
+    }else if(strcmp(token,"substr")==0){
       return SUBSTRA;
-    }else  if(strcmp(s->str,"then")==0){
+    }else  if(strcmp(token,"then")==0){
       return THEN;
-    }else if(strcmp(s->str,"while")==0){
+    }else if(strcmp(token,"while")==0){
       return WHIL;
     }else{
     return ID;
@@ -165,9 +203,12 @@ while(1){
     break;
   case 3:
     if (isdigit(c)){
-      String_Add_Char_Lower(s,c);
+      token[i]=tolower(c);
+      i++;;
       state=3;
     }else if(c=='.'){
+      token[i]=c;
+      i++;
     state=7;
     }else if (c=='+' ||c=='-' ||c=='*' ||c=='/' ||c=='\\' ||c==' '){
     state=0;
@@ -179,9 +220,12 @@ while(1){
     break;
   case 4:
     if(c=='"'){
+      token[i]=c;
+      i++;
       return TEXT;
     }else{
-    String_Add_Char(s,c);
+    token[i]=c;
+    i++;
     state= 4;
     }
     break;
@@ -208,6 +252,8 @@ while(1){
     break;
   case 7:
     if(isdigit(c)){
+      token[i]=c;
+      i++;
       state= 7;
     }else if(c=='+'|| c=='-' || c=='*' || c=='/' || c=='\\'){
     state=0;
